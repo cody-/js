2ee00a55a32f359db70731d8435990897a24a97f
Anton Usmansky <anton.usmansky@gmail.com>
2013-11-02 18:58:18 +0200

    KMP substring search algorythm

---
+++ algo/kmp_search.js
@@ -0,0 +1,38 @@
+// Realization of Knuth-Morris-Pratt algorythm for substring searching
+
+require("./array_ext");
+
+///
+function mkDfa(pattern) {
+	var dfa = {},
+		i, j, X, arr;
+
+	for (i = 0; i < pattern.length; ++i) {
+		arr = new Array(pattern.length);
+		arr.assign(0);
+		dfa[pattern[i]] = arr;
+	}
+
+	dfa[pattern[0]][0] = 1;
+	for (i = 1, X = 0; i < pattern.length; ++i) {
+		for (j = 0; j < pattern.length; ++j) {
+			dfa[pattern[j]][i] = dfa[pattern[j]][X];
+		}
+		dfa[pattern[i]][i] = i + 1;
+		X = dfa[pattern[i]][X];
+	}
+
+	return function(char, state) {
+		return dfa.hasOwnProperty(char) ? dfa[char][state] : 0;
+	}
+}
+
+///
+String.prototype.kmpSearch = function(pattern) {
+	var dfa = mkDfa(pattern);
+	for (var i = 0, j = 0; i < this.length && j < pattern.length; ++i) {
+		j = dfa(this[i], j);
+	}
+
+	return j == pattern.length ? i - j : -1;
+}
--- algo/main.js
+++ algo/main.js
@@ -2,11 +2,16 @@ require("./radix_sort");
 require("./merge_sort");
 require("./quick_sort");
 require("./heap_sort");
+require("./kmp_search");
 
-var arr = [10, 20, 30, 20, 0, -1, 1, 100, 100500, 500, 500100, -1000];
-console.log(arr);
+//var arr = [10, 20, 30, 20, 0, -1, 1, 100, 100500, 500, 500100, -1000];
 //arr.radixSort();
 //arr.mergeSort();
 //arr.quickSort();
-arr.heapSort();
-console.log(arr);
+//arr.heapSort();
+//console.log(arr);
+
+var txt = "Knuth-Morris-Pratt",
+	pattern = "Pratt";
+console.log("indexOf: ", txt.indexOf(pattern));
+console.log("kmpSearch: ", txt.kmpSearch(pattern));
